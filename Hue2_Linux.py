c7e3b3d551952e2230a0278075d1b0e0aad8c83b
Nathan Travers <NNathann@pm.me>
2020-07-19 11:41:20 +0100

    - Led grid updates for led count of current channel\n- Added device and channel selection\n- Updated device information getting and storing\n- Got rid of unneeded notes\n- Added device refresh button

--- Hue2_Linux.py
+++ Hue2_Linux.py
@@ -14,100 +14,86 @@ from gi.repository import Gtk
 class Main:
 	def __init__(self):
 		self.builder = Gtk.Builder()
-		self.builder.add_from_file("LEDtig.glade")
+		self.builder.add_from_file("Hue2_Linux_GUI.glade")
 
 		window = self.builder.get_object("window")
 		window.connect("delete-event", Gtk.main_quit)
 		window.show()
+		
+		device_combo_box = self.builder.get_object("device")
+		device_combo_box.connect("changed", self.onDeviceChanged)
+		
+		device_channel_combo_box = self.builder.get_object("device_channel")
+		device_channel_combo_box.connect("changed", self.onDeviceChannelChanged)
+		
+		def refresh_devices():
+			device_list = self.builder.get_object("device_liststore")
+			device_list.clear()
+			self.devices = self.getHue2Devices()
+			for device in self.devices.keys():
+				device_list.append([device])
+		
+		refresh_button = self.builder.get_object("refresh")
+		refresh_button.connect("clicked", lambda _: refresh_devices())	
+		refresh_devices()
+		
+	def updateLedGrid(self, length):
+		self.led_grid = self.builder.get_object("led_grid")
+		for row in range(5):
+			self.led_grid.remove_row(0)
+		self.led_buttons, x, y = [], -1, 0
+		for led in range(length):
+			self.led_buttons.append(Gtk.ColorButton())
+			x+=1
+			if x==8:
+				y+=1
+				x=0
+			self.led_grid.attach(self.led_buttons[-1],x,y,1,1)
+			self.led_buttons[-1].show()
 
-		device_list = self.builder.get_object("device_liststore")
-		info = self.getHue2Info()
-		for device, channels in info:
-			device_list.append([device])
+		
+	def onDeviceChannelChanged(self, combo_box):
+		try:
+			self.updateLedGrid(self.channels[combo_box.get_child().get_text()])
+		except KeyError:
+			pass
+		
+	def onDeviceChanged(self, combo_box):
+		def updateChannels():
+			channel_list = self.builder.get_object("device_channel_liststore")
+			channel_list.clear()
+			device = combo_box.get_child().get_text()
+			try:
+				self.channels = self.devices[device]
+				for channel in list(self.channels.keys()):
+					channel_list.append([channel])
+				self.updateLedGrid(self.channels["LED 1"])
+			except KeyError:
+				pass
+		updateChannels()
 
-		led_grid = self.builder.get_object("led_grid")
-		self.cl_button = Gtk.ColorButton()
-		self.cl_button.set_size_request(50,0)
-		self.cl_button1 = Gtk.ColorButton()
-		led_grid.attach(self.cl_button,0,0,1,1)
-		self.cl_button.show()
-		led_grid.attach(self.cl_button1,0,1,1,1)
-		self.cl_button1.show()
-
-	def getHue2Info(self):
-		response, channels, channel, devices, count = check_output("liquidctl initialize all", shell=1), [["", 0]], "", [], 0
+	def getHue2Devices(self):
+		response, channel, devices, count = check_output("liquidctl initialize all", shell=1), "", {}, 0
 		for line in response.decode("utf-8").split("\n"):
 			if "N" in line:
-				devices.append([line.split(" (")[0], []])
+				device = line.split(" (")[0]
+				devices[device] = {}
 			try:
 				channel = re.search("LED \d", line).group(0)
-				if channel != channels[-1][0]:
-					channels.append(["", 0])
-					channels[-1][0] = channel
-					devices[-1][1].append(channels[-1])
+				if channel not in devices[device]:
+					devices[device][channel] = 0
 
 				length = int(re.search("\d{3}", line).group(0))
 				if length == 300:
 					count += 10
 				elif length == 250:
 					count += 8
-				channels[-1][1] = channels[-1][1] + count
+				devices[device][channel] = devices[device][channel] + count
 				count = 0
 			except AttributeError:
 				pass
-		del channels		
 		return(devices)
 
 if __name__ == "__main__":
 	main = Main()
 	Gtk.main()
-
-"""
-true_modes = [
-"off",
-"fixed",
-"super-fixed",
-"fading",
-"spectrum-wave",
-"backwards-spectrum-wave",
-"marquee-<length>",
-"backwards-marquee-<length>",
-"covering-marquee",
-"covering-backwards-marquee",
-"alternating-<length>",
-"moving-alternating-<length>",
-"backwards-moving-alternating-<length>",
-"pulse",
-"breathing",
-"super-breathing",
-"candle",
-"starry-night",
-"rainbow-flow",
-"backwards-rainbow-flow",
-"super-rainbow",
-"backwards-super-rainbow",
-"rainbow-pulse",
-"backwards-rainbow-pulse",
-"wings"]
-
-modes = [
-"off",
-"fixed",
-"super-fixed",
-"fading",
-"spectrum-wave",
-"marquee",
-"covering-marquee",
-"alternating",
-"moving-alternating",
-"pulse",
-"breathing",
-"super-breathing",
-"candle",
-"starry-night",
-"rainbow-flow",
-"super-rainbow",
-"rainbow-pulse",
-"wings"]
-"""
-"Static, Marquee/Fading/Pulse/Breathing, Spectrum wave/Rainbow flow/Super rainbow/Rainbow pulse, Alternating"
