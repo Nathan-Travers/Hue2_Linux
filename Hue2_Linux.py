1fe9833d64a00a860e093fedc097be76cc685b5d
Nathan Travers <NNathann@pm.me>
2020-07-20 21:38:43 +0100

    - Created new classes for each stack page - Cleaned up namespace - Added length option for marquee - Marquee now functioning - Slightly increased window size - Added tooltips to some widgets
    To-do:
    - Identify device by number as filtering by name can return multiple results
    - Allow marquee to go backwards
    - Speed slider for breathing on Per_led

--- Hue2_Linux.py
+++ Hue2_Linux.py
@@ -12,117 +12,161 @@ class Main:
 	def __init__(self):
 		self.builder = Gtk.Builder()
 		self.builder.add_from_file("Hue2_Linux_GUI.glade")
-		self.x, self.y = 0, 0
 
 		window = self.builder.get_object("window")
+		device_combo_box = self.builder.get_object("device")
+		device_channel_combo_box = self.builder.get_object("device_channel")
+		self.device_channel_entry = self.builder.get_object("device_channel_entry")
+		refresh_button = self.builder.get_object("refresh")
+		presets_apply = self.builder.get_object("presets_apply")
+
 		window.connect("delete-event", Gtk.main_quit)
+		device_combo_box.connect("changed", self.onDeviceChanged)
+		device_channel_combo_box.connect("changed", self.onDeviceChannelChanged)
+		refresh_button.connect("clicked", lambda _: self.refreshDevices())
+		presets_apply.connect("clicked", lambda _: self.onPresetsApply())
+
+		self.animations_page = Animations(self)
+		self.per_led_page = Per_led(self)
+		self.buttons_to_disable = [self.per_led_page.grouping_cb, self.per_led_page.breathing_cb, self.per_led_page.apply, self.animations_page.apply, presets_apply]
+		self.speeds, self.mode = ["slowest","slower","normal","faster","fastest"], "super-fixed"
+
+		self.refreshDevices()
 		window.show()
 
-		device_combo_box = self.builder.get_object("device")
-		device_combo_box.connect("changed", self.onDeviceChanged)
+	def getHue2Devices(self):
+		response, channel, self.devices, count = check_output("liquidctl initialize all", shell=1), "", {}, 0
+		for line in response.decode("utf-8").split("\n"):
+			if "N" in line:
+				device = line.split(" (")[0]
+				self.devices[device] = {}
+			try:
+				channel = re.search("LED \d", line).group(0).replace("LED ", "led")
+				if channel not in self.devices[device]:
+					self.devices[device][channel] = 0
 
-		device_channel_combo_box = self.builder.get_object("device_channel")
-		device_channel_combo_box.connect("changed", self.onDeviceChannelChanged)
-
-		grouping_cb = self.builder.get_object("grouping_checkbox")
-		grouping_cb.connect("clicked", self.onGroupingCbToggled)
-
-		breathing_cb = self.builder.get_object("breathing_checkbox")
-		breathing_cb.connect("clicked", self.onBreathingCbToggled)
-
-		presets = self.builder.get_object("presets_apply")
-		presets.connect("clicked", lambda _: self.onPresetsApply())
-
-		def onAnimationsApply():
-			for radio_button in self.builder.get_object("radiobutton1").get_group():
-				if radio_button.get_active()==1:
-					self.mode = radio_button.get_label().lower()
-					break
-			colours = []
-			for button in self.custom_colour_buttons:
-				colour = ""
-				for ind, rgb_channel in enumerate(button.get_rgba()):
-					if ind !=3:
-						rgb_channel = int(rgb_channel*255)
-						colour += ("0"*(rgb_channel<10)+format(rgb_channel, "x"))
-				colours.append(colour)
-			self.setLed(*colours)
-		def onRemoveColour(btn):
-			child = ""
-			custom_colours = self.builder.get_object("custom_colours")
-			if self.x==2:
-				self.x, self.y = 1,4
-			if self.y==0 and self.x==1:
-				self.x, self.y = 0, 3
-				child = custom_colours.get_child_at(self.x,self.y)
-			else:
-				child = custom_colours.get_child_at(self.x, self.y-1)
-				self.y-=1
-			del self.custom_colour_buttons[-1]
-			child.destroy()
-			if len(self.custom_colour_buttons)==0:
-				btn.set_sensitive(0)
-
-		remove_colour = self.builder.get_object("remove_colour")
-		remove_colour.connect("clicked", onRemoveColour)
-		animations_apply = self.builder.get_object("animations_apply")
-		animations_apply.connect("clicked", lambda _: onAnimationsApply())
-
-		add_colour = self.builder.get_object("add_colour")
-		add_colour.connect("clicked", lambda _: self.onAddColour(remove_colour))
-
-		self.group_size = 1
-		self.speeds, self.custom_colour_buttons = ["slowest","slower","normal","faster","fastest"], []
-		self.led_grid = self.builder.get_object("led_grid")
-
-		def per_led_apply():
-			colours = []
-			for button in self.led_buttons:
-				colour = ""
-				for ind, rgb_channel in enumerate(button.get_rgba()):
-					if ind !=3:
-						rgb_channel = int(rgb_channel*255)
-						colour += ("0"*(rgb_channel<10)+format(rgb_channel, "x"))
-				for _ in range(self.group_size):
-					colours.append(colour)
-			self.setLed(*colours)
-		per_led_apply_button = self.builder.get_object("per_led_apply")
-		per_led_apply_button.connect("clicked", lambda _: per_led_apply())
-
-		def refresh_devices():
-			device_list = self.builder.get_object("device_liststore")
-			device_entry = self.builder.get_object("device_entry")
-			device_channel_entry = self.builder.get_object("device_channel_entry")
-			device_list.clear()
-			device_entry.set_text("")
-			device_channel_entry.set_text("")
-			self.builder.get_object("grouping_checkbox").set_sensitive(0)
-			self.builder.get_object("breathing_checkbox").set_sensitive(0)
-			self.builder.get_object("presets_apply").set_sensitive(0)
-			self.builder.get_object("per_led_apply").set_sensitive(0)
-			self.builder.get_object("animations_apply").set_sensitive(0)
-			for row in range(5):
-				self.led_grid.remove_row(0)
-
-			self.devices = self.getHue2Devices()
-			for device in self.devices.keys():
-				device_list.append([device])
-		refresh_button = self.builder.get_object("refresh")
-		refresh_button.connect("clicked", lambda _: refresh_devices())
-
-		refresh_devices()
-
+				length = int(re.search("\d{3}", line).group(0))
+				if length == 300:
+					count += 10
+				elif length == 250:
+					count += 8
+				self.devices[device][channel] = self.devices[device][channel] + count
+				count = 0
+			except AttributeError:
+				pass
 
 	def setLed(self, *colours, backwards=0):
-		mode=self.mode
-		system(f"liquidctl -m '{self.device}' set {self.channel} color {('backwards-'*backwards)+mode} --speed={self.speeds[int(self.builder.get_object('speed').get_value())-1]} {' '.join(colours)}")
-		print(f"liquidctl -m '{self.device}' set {self.channel} color {('backwards-'*backwards)+mode} --speed={self.speeds[int(self.builder.get_object('speed').get_value())-1]} {' '.join(colours)}")
+		system(f"liquidctl -m '{self.device}' set {self.channel} color {('backwards-'*backwards)+self.mode} --speed={self.speeds[int(self.builder.get_object('speed').get_value())-1]} {' '.join(colours)}")
+		print(f"liquidctl -m '{self.device}' set {self.channel} color {('backwards-'*backwards)+self.mode} --speed={self.speeds[int(self.builder.get_object('speed').get_value())-1]} {' '.join(colours)}")
+
+	def refreshDevices(self):
+		device_list = self.builder.get_object("device_liststore")
+		device_entry = self.builder.get_object("device_entry")
+
+		device_list.clear()
+		device_entry.set_text("")
+		self.device_channel_entry.set_text("")
+
+		for button in self.buttons_to_disable:
+			button.set_sensitive(0)
+		for row in range(5):
+			self.per_led_page.led_grid.remove_row(0)
+
+		self.getHue2Devices()
+		for device in self.devices.keys():
+			device_list.append([device])
+
+	def onDeviceChannelChanged(self, combo_box):
+		try:
+			self.channel = combo_box.get_child().get_text()
+			self.per_led_page.channel_len = self.channels[self.channel]
+			self.per_led_page.updateLedGrid()
+		except KeyError:
+			pass
+
+	def onDeviceChanged(self, combo_box):
+		def updateChannels():
+			device_channel_list = self.builder.get_object("device_channel_liststore")
+			device_channel_list.clear()
+
+			self.device = combo_box.get_child().get_text()
+			try:
+				self.channels = self.devices[self.device]
+				channel_names = list(self.channels.keys())
+				for channel in channel_names:
+					device_channel_list.append([channel])
+				self.channel=channel_names[0]
+				self.device_channel_entry.set_text(self.channel)
+			except KeyError:
+				pass
+		updateChannels()
+		self.per_led_page.updateLedGrid()
+
+		for button in self.buttons_to_disable:
+			button.set_sensitive(1)
+
+	def onPresetsApply(self):
+		presets_menu = self.builder.get_object("presets_menu")
+		self.mode = presets_menu.get_selected_row().get_children()[0].get_label().lower().replace(" ","-")
+		self.setLed("", backwards=self.builder.get_object("presets_direction_b").get_active())
+
+class Per_led(Main):
+	def __init__(self, top):
+		self.grouping_cb = top.builder.get_object("grouping_checkbox")
+		self.breathing_cb = top.builder.get_object("breathing_checkbox")
+		self.apply = top.builder.get_object("per_led_apply")
+		self.led_grid = top.builder.get_object("led_grid")
+
+		self.grouping_cb.connect("clicked", lambda cb: self.onGroupingCbToggled(top, cb))
+		self.breathing_cb.connect("clicked", lambda cb: self.onBreathingCbToggled(top, cb))
+		self.apply.connect("clicked", lambda _: self.onApply(top))
+
+		self.group_size, self.channel_len = 1, 0
+
+	def onBreathingCbToggled(self, top, cb):
+		if cb.get_active() == 1:
+			top.mode = "super-breathing"
+		else:
+			top.mode = "super-fixed"
+
+	def onGroupingCbToggled(self, top, cb):
+		group_size_entry = top.builder.get_object("group_size")
+		if cb.get_active()==0:
+			group_size_entry.set_visible(0)
+			group_size_entry.disconnect(self.group_size_handler_id)
+			self.group_size=1
+		else:
+			group_size_entry.set_visible(1)
+			self.group_size_handler_id = group_size_entry.connect("changed", self.onGroupSizeChanged)
+			try:
+				top.group_size = int(group_size_entry.get_text())
+			except ValueError:
+				pass
+		self.updateLedGrid()
+
+	def onGroupSizeChanged(self, group_size_entry):
+		group_size, buffer = group_size_entry.get_text(), ""
+		for letter in group_size:
+			if letter in "0123456789":
+				buffer += letter
+		try:
+			buffer_int = int(buffer)
+			if buffer_int > self.channel_len:
+				buffer = str(self.channel_len)
+			elif len(buffer)>2:
+				buffer = buffer[:2]
+			elif buffer_int!=0:
+				self.group_size = buffer_int
+		except ValueError:
+			pass
+		group_size_entry.set_text(buffer)
+		self.updateLedGrid()
 
 	def updateLedGrid(self):
 		for row in range(5):
 			self.led_grid.remove_row(0)
 		self.led_buttons, x, y = [], -1, 0
-		for led in range(ceil(self.channels[self.channel]/self.group_size)):
+		for led in range(ceil(self.channel_len/self.group_size)):
 			self.led_buttons.append(Gtk.ColorButton())
 			x+=1
 			if x==8:
@@ -131,114 +175,85 @@ class Main:
 			self.led_grid.attach(self.led_buttons[-1],x,y,1,1)
 			self.led_buttons[-1].show()
 
-	def onDeviceChannelChanged(self, combo_box):
-		try:
-			self.channel = combo_box.get_child().get_text()
-			self.updateLedGrid()
-		except KeyError:
-			pass
-
-	def onDeviceChanged(self, combo_box):
-		def updateChannels():
-			channel_list = self.builder.get_object("device_channel_liststore")
-			device_channel_entry = self.builder.get_object("device_channel_entry")
-			channel_list.clear()
-			self.device = combo_box.get_child().get_text()
-			try:
-				self.channels = self.devices[self.device]
-				channels = list(self.channels.keys())
-				for channel in channels:
-					channel_list.append([channel])
-				self.channel=channels[0]
-				device_channel_entry.set_text(self.channel)
-				self.updateLedGrid()
-			except KeyError:
-				pass
-		updateChannels()
-		self.builder.get_object("grouping_checkbox").set_sensitive(1)
-		self.builder.get_object("breathing_checkbox").set_sensitive(1)
-		self.builder.get_object("presets_apply").set_sensitive(1)
-		self.builder.get_object("per_led_apply").set_sensitive(1)
-		self.builder.get_object("animations_apply").set_sensitive(1)
+	def onApply(self, top):
+		colours = []
+		for button in self.led_buttons:
+			colour = ""
+			for ind, rgb_channel in enumerate(button.get_rgba()):
+				if ind !=3:
+					rgb_channel = int(rgb_channel*255)
+					colour += ("0"*(rgb_channel<10)+format(rgb_channel, "x"))
+			for _ in range(self.group_size):
+				colours.append(colour)
+		top.setLed(*colours)
 
 
-	def onGroupingCbToggled(self, cb):
-		group_size_entry = self.builder.get_object("group_size")
-		active = cb.get_active()
-		group_size_entry.set_visible(active)
-		if active==0:
-			self.group_size=1
-			group_size_entry.disconnect(self.group_size_handler_id)
+class Animations(Main):
+	def __init__(self, top):
+		remove_colour = top.builder.get_object("remove_colour")
+		self.add_colour = top.builder.get_object("add_colour")
+		self.apply = top.builder.get_object("animations_apply")
+		self.custom_colours = top.builder.get_object("custom_colours")
+		self.marquee_rb = top.builder.get_object("marquee_rb")
+
+		remove_colour.connect("clicked", self.onRemoveColour)
+		self.add_colour.connect("clicked", lambda _: self.onAddColour(remove_colour))
+		self.apply.connect("clicked", lambda _: self.onApply(top))
+		self.marquee_rb.connect("clicked", lambda btn: self.toggleLengthScale(top, btn))
+
+		self.custom_colours.x, self.custom_colours.y, self.custom_colour_buttons = 0, 0, []
+
+	def onRemoveColour(self, btn):
+		child = ""
+		if self.custom_colours.x==2:
+			self.custom_colours.x, self.custom_colours.y = 1,4
+		if self.custom_colours.y==0 and self.custom_colours.x==1: #y before x for lazy evaluation
+			self.custom_colours.x, self.custom_colours.y = 0, 3
+			child = self.custom_colours.get_child_at(self.custom_colours.x,self.custom_colours.y)
 		else:
-			self.group_size_handler_id = group_size_entry.connect("changed", self.onGroupSizeChanged)
-			try:
-				self.group_size = int(group_size_entry.get_text())
-			except ValueError:
-				pass
-		self.updateLedGrid()
+			child = self.custom_colours.get_child_at(self.custom_colours.x, self.custom_colours.y-1)
+			self.custom_colours.y-=1
+		del self.custom_colour_buttons[-1]
+		child.destroy()
 
-	def onBreathingCbToggled(self, cb):
-		if cb.get_active() == 1:
-			self.mode = "super-breathing"
-		else:
-			self.mode = "super-fixed"
-		print("!HD")
-
-	def onGroupSizeChanged(self, group_size_entry):
-		group_size, self.group_size, buffer = group_size_entry.get_text(), 1, ""
-		for letter in group_size:
-			if letter in "0123456789":
-				buffer += letter
-		try:
-			if int(buffer) > self.channels[self.channel]:
-				buffer = str(self.channels[self.channel])
-			elif len(buffer)>2:
-				buffer = buffer[:2]
-			elif int(buffer)!=0:
-				self.group_size = int(buffer)
-		except ValueError:
-			pass
-		group_size_entry.set_text(buffer)
-		self.updateLedGrid()
-
-	def onPresetsApply(self):
-		presets_menu = self.builder.get_object("presets_menu")
-		self.mode = presets_menu.get_selected_row().get_children()[0].get_label().lower().replace(" ","-")
-		self.setLed("", backwards=self.builder.get_object("presets_direction_b").get_active())
+		if len(self.custom_colour_buttons)==0:
+			btn.set_sensitive(0)
 
 	def onAddColour(self, btn):
-		if self.x!=2:
-			custom_colours = self.builder.get_object("custom_colours")
+		if self.custom_colours.x!=2:
 			self.custom_colour_buttons.append(Gtk.ColorButton())
-			custom_colours.attach(self.custom_colour_buttons[-1],self.x,self.y,1,1)
+			self.custom_colours.attach(self.custom_colour_buttons[-1],self.custom_colours.x,self.custom_colours.y,1,1)
 			self.custom_colour_buttons[-1].show()
-			self.y+=1
-			if self.y==4:
-				self.y=0
-				self.x+=1
+			self.custom_colours.y+=1
+			if self.custom_colours.y==4:
+				self.custom_colours.y=0
+				self.custom_colours.x+=1
 			btn.set_sensitive(1)
 
-	def getHue2Devices(self):
-		response, channel, devices, count = check_output("liquidctl initialize all", shell=1), "", {}, 0
-		for line in response.decode("utf-8").split("\n"):
-			if "N" in line:
-				device = line.split(" (")[0]
-				devices[device] = {}
-			try:
-				channel = re.search("LED \d", line).group(0).replace("LED ", "led")
-				if channel not in devices[device]:
-					devices[device][channel] = 0
+	def toggleLengthScale(self, top, btn):
+		length_scale = top.builder.get_object("length_scale")
+		length_scale.set_visible(btn.get_active())
 
-				length = int(re.search("\d{3}", line).group(0))
-				if length == 300:
-					count += 10
-				elif length == 250:
-					count += 8
-				devices[device][channel] = devices[device][channel] + count
-				count = 0
-			except AttributeError:
-				pass
-		return(devices)
+	def onApply(self, top):
+		length=int(top.builder.get_object('length').get_value())
+		for radio_button in top.builder.get_object("animations_mode_rb").get_group():
+			if radio_button.get_active()==1:
+				top.mode = radio_button.get_label().lower()
+				break
+		if top.mode=="marquee":
+			if len(self.custom_colour_buttons)<2:
+				top.mode=f"marquee-{length}"
+			else:
+				top.mode=f"covering-marquee"
+		colours = []
+		for button in self.custom_colour_buttons:
+			colour = ""
+			for ind, rgb_channel in enumerate(button.get_rgba()):
+				if ind !=3:
+					rgb_channel = int(rgb_channel*255)
+					colour += ("0"*(rgb_channel<10)+format(rgb_channel, "x"))
+			colours.append(colour)
+		top.setLed(*colours)
 
 if __name__ == "__main__":
 	main = Main()
